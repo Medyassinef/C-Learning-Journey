cf9f6e8ebacf6359dbb077729d7f9225a58bdae3
Yes_in <137238320+Medyassinef@users.noreply.github.com>
2024-10-14 09:17:52 +0100

    C

--- 0x00-basics/3-RECTANGLE-PERIM-AREA-CALCULATOR.c
+++ 0x00-basics/3-RECTANGLE-PERIM-AREA-CALCULATOR.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 int main (){
+	
 	float length,width;
 	float area,perimeter;
+	
 	printf("Enter the length of your rectangle: \n");
 	scanf("%f",&length);
+	
 	printf("Enter the width of your rectangle: \n");
 	scanf("%f",&width);
+	
 	perimeter = (length + width)*2;
 	area = length * width;
+	
 	printf("The permiter of your rectangle is: %.1f\n",perimeter);
 	printf("And the area is: %.1f\n",area);
 
-
-
-
-
-
+	
 	return 0;
 }
